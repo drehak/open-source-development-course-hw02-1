20eb2e5219c58acd91a541419414c8b4a4cc8986
Dusan Klinec <dusan.klinec@gmail.com>
2020-01-07 11:54:06 +0100

    Add actions, implement missing features and matrix, add tests

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![Build status](https://github.com/drehak/open-source-development-course-hw02-1/actions/workflows/python-package.yml/badge.svg)
+
 # Simple Vector implementation in python 
 
 Very simple vector implementation supporting basic operations.
@@ -5,14 +7,29 @@ Very simple vector implementation supporting basic operations.
 ## Usage
 
 ```python
-from ossdev import Vector
+from ossdev import Vector, Matrix
 a = Vector([0, 1, 2, 3])
 print(a)
+
+m = Matrix.ident(4)
+print(m)
+print(m + m)
 ```
 
 Operations:
-- Addition with a scalar `a+1`
+- Field access: `a[1]`
+- Addition with a scalar: `a + 1`
+- Multiplication with a scalar: `a * 3`
+- XOR with a scalar: `a ^ 3`
+- Vector subtraction: `a - b`
 - Vector addition: `a + b`
+- Vector comparison: `a < b`
+- Vector negation: `-a`
+- Vector length: `length(a)`
+- Vector reversal: `reversed(a)`
+
+Matrix operations:
+- Addition
 
 ## Installation
 
--- ossdev/tests/vct_test.py
+++ ossdev/tests/vct_test.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # Author: ossdev
 
+import math
 import unittest
 from ossdev import Vector
 
@@ -12,12 +13,49 @@ class VectorTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(VectorTest, self).__init__(*args, **kwargs)
 
-    def test_add(self):
+    def test_math(self):
         a = Vector([0, 1, 2, 3])
         b = Vector([3, 2, 1, 0])
         c = a + b
+        d = a - b
+        e = a * 3
+        f = a ^ 2
 
         self.assertEqual(c.get(), [3, 3, 3, 3])
+        self.assertEqual(d.get(), [-3, -1, 1, 3])
+        self.assertEqual(e.get(), [0, 3, 6, 9])
+        self.assertEqual(f.get(), [2, 3, 0, 1])
+
+        self.assertEqual(a.length(), math.sqrt(14))
+
+    def test_setitem(self):
+        v = Vector([0, 1, 2, 3])
+        v[1] = 'a'
+
+        self.assertEqual(v.get(), [0, 'a', 2, 3])
+
+    def test_reversed(self):
+        v = Vector([0, 1, 2, 3])
+
+        self.assertEqual(reversed(v), [3, 2, 1, 0])
+
+    def test_cmp(self):
+        u = Vector([0, 1, 2, 3])
+        v = Vector([0, 1, 2, 4])
+
+        self.assertGreater(v, u)
+        self.assertLess(u, v)
+        self.assertNotEqual(u, v)
+        self.assertEqual(u, u)
+
+    def test_dot(self):
+        a = Vector([1, 2])
+        b = Vector([2, -1])
+        self.assertEqual(a.dot(b), 0)
+
+    def test_lenght(self):
+        self.assertAlmostEqual(Vector([2, 4]).length(), 4.47213595499, 3)
+        self.assertAlmostEqual(Vector([2, 4, 5]).length(), 6.708203932499369, 3)
 
 
 if __name__ == "__main__":
